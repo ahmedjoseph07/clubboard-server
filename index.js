dcb283644e6bae9d7f128efaa280c120f779d18c
ahmedjoseph07 <ahmedjoseph11@gmail.com>
2025-02-07 17:44:13 +0600

    database connection

--- index.js
+++ index.js
@@ -2,4 +2,25 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+app.use(bodyParser.json({limit:"25mb",extended:true}));
+app.use(bodyParser.urlencoded({limit:"25mb",extended:true}));
+app.use(cors());
+const DATABASE_URL = process.env.DATABASE_URL;
+const PORT = process.env.PORT || 4000;
+
+mongoose.connect(DATABASE_URL)
+.then(()=>{
+    app.listen(PORT,()=>{
+        console.log(`Server running on ${PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("Database Error",err.message);
+})
+
 
